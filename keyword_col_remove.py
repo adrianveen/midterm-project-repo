357884f8e2fc535c25baab07f14976085aee1fad
Adrian <adrian.veen@gmail.com>
2023-11-30 00:24:41 -0500

    many additions/models ran in modelling-adrian notebook. Added heatmap for per capita values

--- keyword_col_remove.py
+++ keyword_col_remove.py
@@ -1,9 +1,9 @@
-def keyword_col_remove(df, to_remove):
+def keyword_col_remove(df, keywords):
     """Removes columns with the specified keywords."""
     
     df = df.copy()
 
-    columns_to_remove = [col for col in df.columns if any(word in col for word in to_remove)]
+    columns_to_remove = [col for col in df.columns if any(word in col for word in keywords)]
 
     df = df.drop(columns_to_remove, axis=1)
 
--- proj_functions.py
+++ proj_functions.py
@@ -9,3 +9,17 @@ def remove_null_gdp(df):
     df = df.loc[~df['country'].isin(countries_with_zero), :]
     
     return df
+
+#iterate through a model and X dataframe to remove variables with high p-values
+# one at a time until all variables have a p-value below 0.05
+def remove_high_pvalue(model, X):
+    while model.pvalues.max() > 0.05:
+        p_values = model.pvalues
+        p_values = p_values.drop('const')
+        p_values = p_values[p_values != p_values.max()]
+        p_values = p_values.index.tolist()
+        X = X[p_values]
+        X = sm.add_constant(X)
+        lin_reg = sm.OLS(y, X)
+        model = lin_reg.fit()
+    return model
