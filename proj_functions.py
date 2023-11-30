cad99b80a25f2eb3ab5a1e3c776b19d6a209295e
Adrian <adrian.veen@gmail.com>
2023-11-29 20:33:28 -0500

    recent updates to cleanedData_v2.csv, small change to adrian's cleaning notebookk

--- proj_functions.py
+++ proj_functions.py
@@ -1,3 +1,11 @@
 from remove_zeros import remove_zeros
 from keyword_col_remove import keyword_col_remove
 from year_filter import year_filter
+
+#remove 0 gdp values
+def remove_null_gdp(df):
+
+    countries_with_zero = [index for index, value in df.groupby('country')['gdp'].count().sort_values().items() if value == 0]
+    df = df.loc[~df['country'].isin(countries_with_zero), :]
+    
+    return df
